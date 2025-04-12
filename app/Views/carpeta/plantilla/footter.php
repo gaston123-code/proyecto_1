f7204ff305bb5451fdb5b8accfe3430d46029560
Gastón Francisco Encinas <prueba@correo.com>
2025-04-12 15:03:55 -0300

    ya hice el footer

--- app/Views/carpeta/plantilla/footter.php
+++ app/Views/carpeta/plantilla/footter.php
@@ -1,26 +1,39 @@
 <footer class="container-fluid">
       <div class="row">
         <div class="col">
-        copyright
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Direccion de email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">Nunca compartiremos tu email con nadie mas.</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+          </form>
         </div>
-        <div class="col">
-        copyright
+        <div class="col tex-align-end">
+        <ul class="list-group lista">
+            <li class="list-group-item border-0"><a href="#">Talles</a></li>
+            <li class="list-group-item  border-0"><a href="#">Promociones de compras</a></li>
+            <li class="list-group-item border-0"><a href="#">Politicas de cambio</a></li>
+            <li class="list-group-item border-0"><a href="#">Defensa del consumidor</a></li>
+          </ul>
         </div>
-        <div class="col">
-        <ul class="list-group list-group-flush">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-</ul>
+        <div class="col tex-align-center">
+          <ul class="list-group lista">
+            <li class="list-group-item border-0"><a href="#">Preguntas frecuentas</a></li>
+            <li class="list-group-item  border-0"><a href="#">Politicas de privacidad</a></li>
+            <li class="list-group-item border-0"><a href="#">Garantias</a></li>
+            <li class="list-group-item border-0"><a href="#">Politicas de debolución</a></li>
+          </ul>
         </div>
         <div class="col tex-align-end">
             <ul class="list-group  list-group-horizontal justify-content-center" >
-                <li class="list-group-item  text-decoration-none border-0" ><a href="#"><i class="fa-brands fa-instagram fa-3x"></i></a></li>
-                <li class="list-group-item text-decoration-none border-0" ><a href="#"><i class="fa-brands fa-square-facebook fa-3x"></i></a></li>
-                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-x-twitter fa-3x"></i></a></li>
-                <li class="list-group-item  text-decoration-none border-0" ><a><i class="fa-brands fa-youtube fa-3x"></i></a></li>
-                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-tiktok fa-3x"></i></a></li>
-                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-linkedin fa-3x"></i></a></li>
+                <li class="list-group-item  text-decoration-none border-0" ><a href="#"><i class="fa-brands fa-instagram fa-2x"></i></a></li>
+                <li class="list-group-item text-decoration-none border-0" ><a href="#"><i class="fa-brands fa-square-facebook fa-2x"></i></a></li>
+                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-x-twitter fa-2x"></i></a></li>
+                <li class="list-group-item  text-decoration-none border-0" ><a><i class="fa-brands fa-youtube fa-2x"></i></a></li>
+                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-tiktok fa-2x"></i></a></li>
+                <li class="list-group-item text-decoration-none border-0" ><a><i class="fa-brands fa-linkedin fa-2x"></i></a></li>
             </ul>
 
         </div>
